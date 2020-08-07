62f92b2aaac1431853981ef2058d4068f88958af
ZipCock <20wlsgml@gmail.com>
2020-08-07 13:43:27 +0900

    Added gitignore

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const axios = require("axios");
 
 const app = express();
 
+const config = require("./config.json");
+
 app.use(cors());
 app.use(express.json());
 
@@ -11,6 +13,7 @@ app.get("/", (req, res) => {
   res.send("첫 페이지 응답값");
 });
 
+// 빌링키 발급
 app.post("/subscribe/key", async (req, res) => {
   const {
     cardNum: card_number,
@@ -22,9 +25,8 @@ app.post("/subscribe/key", async (req, res) => {
 
   try {
     const getToken = await axios.post("https://api.iamport.kr/users/getToken", {
-      imp_key: "6906115296610656", // REST API키
-      imp_secret:
-        "H0gXk8iW5iAfq9unN5vOK0rFrNIRVEiCFyzZHSWNbw2BGwYuCuimvK1WxJOto653EE3mxZ9IvbkcIjGk", // REST API Secret
+      imp_key: config.REST_KEY, // REST API키
+      imp_secret: config.REST_SECRET, // REST API Secret
     });
 
     const { access_token } = getToken.data.response;

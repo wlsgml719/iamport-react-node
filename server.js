7838a0570b0f40e1306fbc7a171a7e4f367e43a4
ZipCock <20wlsgml@gmail.com>
2020-08-07 13:36:34 +0900

    Added billing key request

--- server.js
+++ server.js
@@ -1,17 +1,68 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("첫 페이지 응답값");
 });
 
-app.post("/subscribe/key", (req, res) => {
-  console.log(req);
-  res.json({ result: "응답값" });
+app.post("/subscribe/key", async (req, res) => {
+  const {
+    cardNum: card_number,
+    expiry,
+    birth,
+    pwd2Digit: pwd_2digit,
+    customer_uid,
+  } = req.body;
+
+  try {
+    const getToken = await axios.post("https://api.iamport.kr/users/getToken", {
+      imp_key: "6906115296610656", // REST API키
+      imp_secret:
+        "H0gXk8iW5iAfq9unN5vOK0rFrNIRVEiCFyzZHSWNbw2BGwYuCuimvK1WxJOto653EE3mxZ9IvbkcIjGk", // REST API Secret
+    });
+
+    const { access_token } = getToken.data.response;
+
+    // console.group("getToken");
+    // console.log(access_token);
+    // console.groupEnd("getToken");
+
+    const issueBilling = await axios.post(
+      `https://api.iamport.kr/subscribe/customers/${customer_uid}`,
+      {
+        card_number,
+        expiry,
+        birth,
+        pwd_2digit,
+      },
+      { headers: { Authorization: access_token } } // 인증 토큰 Authorization header에 추가
+    );
+
+    const { code, message } = issueBilling.data;
+
+    // console.group("issueBilling");
+    // console.log(code, message);
+    // console.groupEnd("issueBilling");
+
+    if (code === 0) {
+      // 빌링키 발급 성공
+      res.send({
+        status: "success",
+        message: `Billing has successfully issued, check customer_uid, ${customer_uid}`,
+      });
+    } else {
+      // 빌링키 발급 실패
+      res.send({ status: "failed", message });
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
 
 const port = 3002;
